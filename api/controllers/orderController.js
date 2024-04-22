d58437898156a27101cee94f6fdc7a0901f97d8b
Balqees <balqeesab81@gmail.com>
2024-04-22 13:37:08 +0300

    update order

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -8,58 +8,39 @@ exports.getAllOrder = factory.getAllpop1(
   Order,
   {
     path: 'user',
-    select: 'name phone -_id',
+    select: 'name  -_id',
   },
   {
     path: 'cart.product',
     select: 'name price -_id',
   },
-  {
-    path: 'delivery',
-    select: 'firstName lastName phone -_id',
-  },
-  {
-    path: 'location',
-  }
 );
 exports.getOrder = factory.getOne(
   Order,
   {
     path: 'user',
-    select: 'name phone -_id',
+    select: 'name -_id',
   },
   {
     path: 'cart.product',
     select: 'name price -_id',
   },
-  {
-    path: 'delivery',
-    select: 'firstName lastName phone -_id',
-  },
-  {
-    path: 'location',
-  },
-  {
-    path: 'restaurant',
-    select: 'name -_id',
-  }
 );
 exports.updateOrder = factory.updateOne(Order);
 exports.deleteOrder = factory.deleteOne(Order);
 exports.createOrder = factory.createOne(Order);
-exports.chekOrder = catchAsync(async (req, res, next) => {
-  const doc = await Order.findById(req.params.id);
-  if (doc.status != ('Accepted' || 'Placed'))
-    return next(new AppError('the order Preparing', 400));
-  next();
-});
+// exports.chekOrder = catchAsync(async (req, res, next) => {
+//   const doc = await Order.findById(req.params.id);
+//   if (doc.status != ('Accepted' || 'Placed'))
+//     return next(new AppError('the order Preparing', 400));
+//   next();
+// });
 
 exports.statisticsWithLinkUser = factory.statisticsWithLink(
   Order,
-  'priceDelivery',
+  'total',
   'users',
   'user',
   'user.name',
-  'user.phone',
   'user.email'
 );
